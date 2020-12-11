c46ad722646ac6c9ed234ed113963cd2b134b54d
spaceC00kie <kirstie317@gmail.com>
2020-12-10 20:57:23 -0500

    reverse another string

--- practice06.js
+++ practice06.js
@@ -1,11 +1,21 @@
 //reverse a string
 
-const backwards = (word) => {
-    let x = ""
-    for (let i = word.length-1; i >= 0; i--){
-        x += word[i]
+//const backwards = (word) => {
+//    let x = ""
+//    for (let i = word.length-1; i >= 0; i--){
+//        x += word[i]
+//    }
+//    return x
+//}
+
+//console.log(backwards("reverse"))
+
+const rewind = (str) => {
+    let r = ""
+    for (let i = str.length-1; i >= 0; i--){
+        r += str[i]
     }
-    return x
+    return r
 }
 
-console.log(backwards("reverse"))
+console.log(rewind("this"))
